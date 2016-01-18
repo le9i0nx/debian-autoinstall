db2091ec79792161a8eace05904a3ab4cd74cbf3
Gavrilov Alexy <le9i0nx@gmail.com>
2016-01-18 12:06:32 +0500

    add build_iso.sh

---
+++ build_iso.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+mkdir iso
+cd iso
+wget -O linux -c "http://ftp.debian.org/debian/dists/stable/main/installer-amd64/current/images/netboot/debian-installer/amd64/linux"
+wget -O initrd.gz -c "http://ftp.debian.org/debian/dists/stable/main/installer-amd64/current/images/netboot/debian-installer/amd64/initrd.gz"
+wget -O isolinux.bin -c "http://mirror.centos.org/centos/7/os/x86_64/isolinux/isolinux.bin"
+
+cat > isolinux.cfg << EOF
+prompt 0
+timeout 0
+default auto
+
+label auto
+    kernel /linux
+    append auto=true url=http://le9i0nx.github.io/debian-autoinstall/preseed.cfg keyboard-configuration/xkb-keymap=en priority=critical vga=788 initrd=/initrd.gz --- quiet
+EOF
+cd ..
+genisoimage -r -V "debian auto install" -cache-inodes -J -l -b isolinux.bin  -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o debian-stable-amd64-auto-install.iso iso/
+
