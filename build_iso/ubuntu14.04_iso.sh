717aa71fb6ce77e8c59393edd43174403b7c763d
Gavrilov Alexy <le9i0nx@gmail.com>
2016-04-22 14:01:07 +0500

    add build_iso ubuntu16.04

---
+++ build_iso/ubuntu14.04_iso.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+ROOT_PATH=$(cd $(dirname $0) && pwd);
+echo $ROOT_PATH;
+
+mkdir -p /tmp/iso
+mkdir -p /tmp/ubuntu14.04
+cd /tmp/ubuntu14.04
+[ -f initrd.gz ] || wget "http://archive.ubuntu.com/ubuntu/dists/trusty/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/initrd.gz"
+[ -f firmware.cpio.gz ] || wget "http://cdimage.debian.org/cdimage/unofficial/non-free/firmware/stable/current/firmware.cpio.gz"
+cat initrd.gz firmware.cpio.gz > /tmp/iso/initrd.gz
+[ -f linux ] || wget "http://archive.ubuntu.com/ubuntu/dists/trusty/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/linux"
+cat linux > /tmp/iso/linux
+[ -f isolinux.bin ] || wget "http://mirror.centos.org/centos/7/os/x86_64/isolinux/isolinux.bin"
+cat isolinux.bin > /tmp/iso/isolinux.bin
+
+cat > /tmp/iso/isolinux.cfg << EOF
+prompt 0
+timeout 0
+default auto
+
+label auto
+    kernel /linux
+    append auto=true url=http://le9i0nx.github.io/debian-autoinstall/ubuntu.cfg keyboard-configuration/xkb-keymap=en priority=critical vga=788 initrd=/initrd.gz --- quiet
+EOF
+
+cd ..
+genisoimage -r -V "ubuntu auto install" -cache-inodes -J -l -b isolinux.bin  -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ubuntu-14.04-amd64-auto-install.iso iso/
+rm -r /tmp/iso
--- build_iso/ubuntu16.04_iso.sh
+++ build_iso/ubuntu16.04_iso.sh
@@ -3,11 +3,12 @@ ROOT_PATH=$(cd $(dirname $0) && pwd);
 echo $ROOT_PATH;
 
 mkdir -p /tmp/iso
-cd /tmp/ubuntu
-[ -f initrd.gz ] || wget "http://archive.ubuntu.com/ubuntu/dists/trusty/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/initrd.gz"
+mkdir -p /tmp/ubuntu16.04
+cd /tmp/ubuntu16.04
+[ -f initrd.gz ] || wget "http://archive.ubuntu.com/ubuntu/dists/xenial/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/initrd.gz"
 [ -f firmware.cpio.gz ] || wget "http://cdimage.debian.org/cdimage/unofficial/non-free/firmware/stable/current/firmware.cpio.gz"
 cat initrd.gz firmware.cpio.gz > /tmp/iso/initrd.gz
-[ -f linux ] || wget "http://archive.ubuntu.com/ubuntu/dists/trusty/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/linux"
+[ -f linux ] || wget "http://archive.ubuntu.com/ubuntu/dists/xenial/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/linux"
 cat linux > /tmp/iso/linux
 [ -f isolinux.bin ] || wget "http://mirror.centos.org/centos/7/os/x86_64/isolinux/isolinux.bin"
 cat isolinux.bin > /tmp/iso/isolinux.bin
@@ -18,10 +19,10 @@ timeout 0
 default auto
 
 label auto
-    kernel /linux
+    kernel /linux 
     append auto=true url=http://le9i0nx.github.io/debian-autoinstall/ubuntu.cfg keyboard-configuration/xkb-keymap=en priority=critical vga=788 initrd=/initrd.gz --- quiet
 EOF
 
 cd ..
-genisoimage -r -V "ubuntu auto install" -cache-inodes -J -l -b isolinux.bin  -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ubuntu-lts-amd64-auto-install.iso iso/
+genisoimage -r -V "ubuntu auto install" -cache-inodes -J -l -b isolinux.bin  -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ubuntu-16.04-amd64-auto-install.iso iso/
 rm -r /tmp/iso
