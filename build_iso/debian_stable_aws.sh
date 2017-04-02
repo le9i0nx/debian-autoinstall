7295b813c238c5c751ace71c92c5c28920e5104d
Gavrilov Aleksej <le9i0nx@gmail.com>
2017-04-02 18:55:15 +0500

    fix aws install

--- build_iso/debian_stable_aws.sh
+++ build_iso/debian_stable_aws.sh
@@ -3,7 +3,7 @@ mkdir -p /tmp/debian
 cd /tmp/debian
 [ -f initrd.gz ] || wget "http://ftp.debian.org/debian/dists/stable/main/installer-amd64/current/images/netboot/debian-installer/amd64/initrd.gz"
 [ -f firmware.cpio.gz ] || wget "http://cdimage.debian.org/cdimage/unofficial/non-free/firmware/stable/current/firmware.cpio.gz"
-cat initrd.gz firmware.cpio.gz > /boot/initrd.gz
+cat initrd.gz firmware.cpio.gz > /initrd.gz
 rm -r /tmp/debian
 cd /boot
 [ -f linux ] || wget "http://ftp.debian.org/debian/dists/stable/main/installer-amd64/current/images/netboot/debian-installer/amd64/linux"
@@ -12,8 +12,9 @@ cat > /boot/grub/grub.cfg << EOF
 timeout=1
 
 menuentry 'auto install debian' {
-    linux    /boot/linux auto=true url=http://le9i0nx.github.io/debian-autoinstall/aws.cfg interface=auto netcfg/dhcp_timeout=60 keyboard-configuration/xkb-keymap=en priority=critical vga=788 --- quiet
-    initrd   /boot/initrd.gz
+    search --set=root /linux
+    linux    /linux auto=true url=http://le9i0nx.github.io/debian-autoinstall/aws.cfg interface=auto netcfg/dhcp_timeout=60 keyboard-configuration/xkb-keymap=en priority=critical vga=788 --- quiet
+    initrd   /initrd.gz
 }
 
 EOF
