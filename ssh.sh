9f44f500bf9083838ee6db0fa741d6416145a723
Aleksey Gavrilov <le9i0nx@gmail.com>
2017-06-14 10:15:56 +0500

    Create ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -2,4 +2,5 @@
 mkdir /root/.ssh
 chmod 0700 /root/.ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC753PqNZfK44HWt5QyrOSVZi3/mgqTKeEoXmS7Ej0nXPWqRhRnm+SQg+hbNEAgomB7VhLPrZP5mlXWfyKebB/QRZ3CZoKdfqEZKVaqfoYoC6P/f5sQ8X7NqMKc+ecozQtIR3IaCfjBpT3Nm+3NCOXF9dOHhY8I6GucoHOmGD3swCqLpYQO9PoCRCdWZicHIgnUVqXjbCUR+fTVWeXSkthF+afCBx3jwOunzDm6FiiLezsduKyvWTNv/J1eHaFQjJF/YCVTtEKK21XI/VSuoN6eUJdduEKC7rtiKW4eaAXNWPzPRuPhxzbMys66EefpLxAISPA/eu3AzMZWGz/lgA8j root@local" >> /root/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAg17DPi2Di60zH4DPH2xjCx3bbiHs8DFrER8hLxZJpd root@nout" >> /root/.ssh/authorized_keys
 chmod 0600 /root/.ssh/authorized_keys
