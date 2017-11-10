39f75447f600df94da1908e16efbfa4c9d1de8a6
Aleksey Gavrilov <le9i0nx@gmail.com>
2017-11-10 12:56:03 +0500

    fix mkdir: cannot create directory ‘/root/.ssh’: File exists

--- ssh.sh
+++ ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 TARGET=$1
-mkdir $TARGET/root/.ssh
+mkdir -p $TARGET/root/.ssh
 chmod 0700 $TARGET/root/.ssh
 wget --quiet -O - https://le9i0nx.github.io/debian-autoinstall/authorized_keys >> $TARGET/root/.ssh/authorized_keys
 chmod 0600 $TARGET/root/.ssh/authorized_keys
